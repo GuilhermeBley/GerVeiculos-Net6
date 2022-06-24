667433695b10718b6e1f17d0d335175f2f6b8e4a
Bley <bleygui@gmail.com>
2022-06-24 01:24:28 -0300

    View Login and Register

--- ClienteNet6/Shared/Dto/LoginCreate.cs
+++ ClienteNet6/Shared/Dto/LoginCreate.cs
@@ -1,9 +1,22 @@
-﻿namespace ClienteNet6.Shared.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClienteNet6.Shared.Dto
 {
     public class LoginCreate
     {
+        [Required(ErrorMessage = "Nome da empresa é obrigatório.")]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "Nome de empresa inválido.")]
         public string NomeEmpresa { get; set; }
+
+        [Required(ErrorMessage = "E-mail é obrigatório.")]
+        [EmailAddress(ErrorMessage ="Email inválido.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatório.")]
+        [StringLength(24, MinimumLength = 8, ErrorMessage ="Senha deve conter entre 8 e 24 caracteres.")]
         public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage ="Confirmação de senha inválida.")]
+        public string ConfirmPassword { get; set; }
     }
 }
--- ClienteNet6/Shared/Dto/LoginUser.cs
+++ ClienteNet6/Shared/Dto/LoginUser.cs
@@ -1,8 +1,15 @@
-﻿namespace ClienteNet6.Shared.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClienteNet6.Shared.Dto
 {
     public class LoginUser
     {
+        [Required(ErrorMessage ="Usuário obrigatório.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Senha obrigatória.")]
+        [StringLength(24, MinimumLength = 8, ErrorMessage = "Senha deve conter entre 8 e 24 caracteres.")]
         public string Password { get; set; }
     }
 }
