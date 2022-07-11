7acc1878ec91062683d3844b92565f6617ae1499
GuilhermeBley <guilhermecbley@gmail.com>
2022-07-11 15:01:32 -0300

    modal finished

---
+++ ClienteNet6/Client/wwwroot/js/modal.js
@@ -0,0 +1,17 @@
+﻿const modalNames = 'modal-open';
+
+function OpenModal() {
+    var bodys = document.getElementsByTagName('body');
+
+    for (var i = 0; i < bodys.length; i++) {
+        bodys[i].classList.remove(modalNames);
+    }
+}
+
+function RemoveModal() {
+    var bodys = document.getElementsByTagName('body');
+
+    for (var i = 0; i < bodys.length; i++) {
+        bodys[i].classList.add(modalNames);
+    }
+}
